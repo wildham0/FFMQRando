83ddaf5d94683e277113ebad9b692eea1841acb8
wildham0 <64616385+wildham0@users.noreply.github.com>
2022-03-22 08:53:16 -0400

    Fix things up

--- FFMQRLib/FFMQRom.cs
+++ FFMQRLib/FFMQRom.cs
@@ -10,7 +10,7 @@ namespace FFMQLib
 {
 	public static class Metadata
 	{
-		public static string VersionNumber = "0.3.991";
+		public static string VersionNumber = "0.3.05";
 		public static string Version = VersionNumber + "-beta";
 	}
 	
