3e606e59158d7bc360302110c3331cd3b81db8ae
wildham0 <64616385+wildham0@users.noreply.github.com>
2022-05-29 19:07:28 -0400

    version test

--- FFMQRLib/FFMQRom.cs
+++ FFMQRLib/FFMQRom.cs
@@ -10,7 +10,7 @@ namespace FFMQLib
 {
 	public static class Metadata
 	{
-		public static string VersionNumber = "0.3.26";
+		public static string VersionNumber = "0.3.27";
 		public static string Version = VersionNumber + "-beta";
 	}
 	
