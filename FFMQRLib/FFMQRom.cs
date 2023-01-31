3db9fe3c7e0fc399f5822579d25ae356cf2d7507
wildham0 <64616385+wildham0@users.noreply.github.com>
2023-01-31 11:10:14 -0500

    Fix Vanilla crests

--- FFMQRLib/FFMQRom.cs
+++ FFMQRLib/FFMQRom.cs
@@ -10,7 +10,7 @@ namespace FFMQLib
 {
 	public static class Metadata
 	{
-		public static string Version = "1.3.23";
+		public static string Version = "1.3.24";
 	}
 	public partial class FFMQRom : SnesRom
 	{
