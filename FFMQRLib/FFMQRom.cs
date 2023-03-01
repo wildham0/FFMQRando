3670978f553851344f1cc999f710ef04041e500c
wildham0 <64616385+wildham0@users.noreply.github.com>
2023-03-01 09:19:54 -0500

    welp

--- FFMQRLib/FFMQRom.cs
+++ FFMQRLib/FFMQRom.cs
@@ -10,7 +10,7 @@ namespace FFMQLib
 {
 	public static class Metadata
 	{
-		public static string Version = "1.3.42";
+		public static string Version = "1.3.43";
 	}
 	public partial class FFMQRom : SnesRom
 	{
