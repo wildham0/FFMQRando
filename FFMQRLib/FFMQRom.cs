03b6e25c3da1ab20472266727ae17c3d8295c956
wildham0 <64616385+wildham0@users.noreply.github.com>
2023-03-27 22:01:17 -0400

    Update Version

--- FFMQRLib/FFMQRom.cs
+++ FFMQRLib/FFMQRom.cs
@@ -10,7 +10,7 @@ namespace FFMQLib
 {
 	public static class Metadata
 	{
-		public static string Version = "1.4.02";
+		public static string Version = "1.4.03";
 	}
 	public partial class FFMQRom : SnesRom
 	{
