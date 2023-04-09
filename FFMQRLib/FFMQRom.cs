4eb8407eb367d84c9227f9dbab860dc29b1e548e
wildham0 <64616385+wildham0@users.noreply.github.com>
2023-04-09 07:54:19 -0400

    Fix venus chest/safe enemy scripts

--- FFMQRLib/FFMQRom.cs
+++ FFMQRLib/FFMQRom.cs
@@ -10,7 +10,7 @@ namespace FFMQLib
 {
 	public static class Metadata
 	{
-		public static string Version = "1.4.14";
+		public static string Version = "1.4.15";
 	}
 	public partial class FFMQRom : SnesRom
 	{
