e95fe8e11f3463050ab9457a780a918aa3ee6f47
wildham0 <64616385+wildham0@users.noreply.github.com>
2023-05-07 10:15:41 -0400

    Fix crest teleport logic bug

--- FFMQRLib/FFMQRom.cs
+++ FFMQRLib/FFMQRom.cs
@@ -11,7 +11,7 @@ namespace FFMQLib
 {
 	public static class Metadata
 	{
-		public static string Version = "1.4.27";
+		public static string Version = "1.4.28";
 	}
 	public partial class FFMQRom : SnesRom
 	{
