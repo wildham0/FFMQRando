1b42ccd265e156beb35b6b64a9a5ff6db62a1f74
wildham0 <64616385+wildham0@users.noreply.github.com>
2023-06-11 13:00:02 -0400

    Music shuffle fix

--- FFMQRLib/FFMQRom.cs
+++ FFMQRLib/FFMQRom.cs
@@ -11,7 +11,7 @@ namespace FFMQLib
 {
 	public static class Metadata
 	{
-		public static string Version = "1.4.34";
+		public static string Version = "1.4.35";
 	}
 	public partial class FFMQRom : SnesRom
 	{
