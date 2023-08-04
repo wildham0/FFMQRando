5cb6cbd642b8faad04b71981b22ea77347f6249a
wildham0 <64616385+wildham0@users.noreply.github.com>
2023-08-04 11:08:27 -0400

    Add seach box to custom sprites

--- FFMQRLib/Randomize.cs
+++ FFMQRLib/Randomize.cs
@@ -10,7 +10,7 @@ namespace FFMQLib
 {
 	public static class Metadata
 	{
-		public static string Version = "1.5.01";
+		public static string Version = "1.5.02";
 	}
 	public partial class FFMQRom : SnesRom
 	{
