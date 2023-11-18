7cd42ffd9bbfe95d9aa8e8e8117289294e68d4ca
wildham0 <64616385+wildham0@users.noreply.github.com>
2023-11-18 00:04:43 -0500

    Update version

--- FFMQRLib/Randomize.cs
+++ FFMQRLib/Randomize.cs
@@ -10,7 +10,7 @@ namespace FFMQLib
 {
 	public static class Metadata
 	{
-		public static string Version = "1.5.32";
+		public static string Version = "1.5.33";
 	}
 	public partial class FFMQRom : SnesRom
 	{
