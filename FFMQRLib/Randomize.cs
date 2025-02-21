e6c6cf32721de2ee913b018611919e62173d5698
wildham0 <64616385+wildham0@users.noreply.github.com>
2025-02-21 18:23:24 -0500

    Fix prog formation bug

--- FFMQRLib/Randomize.cs
+++ FFMQRLib/Randomize.cs
@@ -14,7 +14,7 @@ namespace FFMQLib
 		// X = Global Version
 		// YY = Release
 		// ZZ = Build
-		public static string Version = "1.06.23";
+		public static string Version = "1.06.24";
 		
 	}
 	public partial class FFMQRom : SnesRom
