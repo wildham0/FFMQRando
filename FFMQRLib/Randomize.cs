d5ba9c54806e94a6ff860981e0ed8c2427aad12b
wildham0 <64616385+wildham0@users.noreply.github.com>
2025-05-30 10:45:30 -0400

    Fix logic not setting locations correctly

--- FFMQRLib/Randomize.cs
+++ FFMQRLib/Randomize.cs
@@ -17,7 +17,7 @@ namespace FFMQLib
 		// Z = Patch Release
 		// Increment Beta on every new builds, reset to zero on version increase
 		public static string Version = "1.6.1";
-		public static string Beta = "01";
+		public static string Beta = "02";
 		public static string BetaVersionShort => Version + "-b" + Beta;
 		public static string BetaVersionLong => Version + "-beta" + Beta;
 	}
