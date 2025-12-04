7a204ce3145890dfd0157bae51878d4289c66b39
wildham0 <64616385+wildham0@users.noreply.github.com>
2025-12-04 18:40:26 -0500

    Fix spencer cave entrance getting shuffled

--- FFMQRLib/Randomize.cs
+++ FFMQRLib/Randomize.cs
@@ -17,7 +17,7 @@ namespace FFMQLib
 		// Z = Patch Release
 		// Increment Beta on every new builds, reset to zero on version increase
 		public static string Version = "1.7.0";
-		public static string Build = "06";
+		public static string Build = "07";
 		public static string BetaVersionShort => Version + "-b" + Build;
 		public static string BetaVersionLong => Version + "-beta" + Build;
 	}
