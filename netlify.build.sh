dabb8dab72b739b15d9570ea84f0b7cd76b00527
wildham0 <64616385+wildham0@users.noreply.github.com>
2025-12-04 19:10:49 -0500

    4th

--- netlify.build.sh
+++ netlify.build.sh
@@ -2,13 +2,12 @@
 set -e
 
 ## install latest .NET 9.0 release
-#pushd /tmp
-#wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
+pushd /tmp
+wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
 #wget https://dot.net/v1/dotnet-install.sh
-#chmod u+x /tmp/dotnet-install.sh
-#/tmp/dotnet-install.sh --channel 9.0
-#popd
-apt install dotnet-sdk-9.0
+chmod u+x /tmp/dotnet-install.sh
+/tmp/dotnet-install.sh --channel 9.0
+popd
 
 ## publish project to known location for subsequent deployment by Netlify
-dotnet build FFMQWebAsm --configuration Release && dotnet publish FFMQWebAsm -c Release --no-build -o publishoutput
+/opt/buildhome/.dotnet/dotnet build FFMQWebAsm --configuration Release && dotnet publish FFMQWebAsm -c Release --no-build -o publishoutput
