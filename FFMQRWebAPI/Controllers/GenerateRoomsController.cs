cd22078b28524b1f8b7de1555b14286da3f5f7fe
wildham0 <64616385+wildham0@users.noreply.github.com>
2025-12-07 22:49:03 -0500

    dynamic load the rando with the api

--- FFMQRWebAPI/Controllers/GenerateRoomsController.cs
+++ FFMQRWebAPI/Controllers/GenerateRoomsController.cs
@@ -1,8 +1,9 @@
-using FFMQLib;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Text;
 using System.Web;
+using System.Reflection;
+using FFMQLib;
 
 
 namespace FFMQRWebAPI.Controllers
@@ -23,12 +24,50 @@ namespace FFMQRWebAPI.Controllers
         {
 			//return StatusCode(StatusCodes.Status426UpgradeRequired, "Your version of AP World is unsupported, upgrade to 1.7 APWorld or set the following feature to default.");
 
-			// cs and km are set for compatibility with 1.4; if AP is updated to 1.5 before, we can remove the default value
-			FFMQRom rom = new FFMQRom();
-            var newrooms = rom.GenerateRooms(c, b, m, cs, km, os, s);
-            
-            var bytes = Encoding.ASCII.GetBytes(newrooms);
-            return File(bytes, "text/plain", "rooms.yaml");
-        }
+			Assembly ffmqlib = Assembly.LoadFrom("dll/1.6/FFMQRLib.dll");
+
+			Type? libType = ffmqlib.GetType("FFMQLib.FFMQRom");
+			if (libType != null)
+			{
+				object? instance = Activator.CreateInstance(libType);
+				if (instance != null)
+				{
+					MethodInfo? method = libType.GetMethod("GenerateRooms");
+					if (method != null)
+					{
+						string? newrooms = (string?)method.Invoke(instance, new object?[] { c, b, m, cs, km, os, s });
+
+						if (newrooms != null)
+						{
+							var bytes = Encoding.ASCII.GetBytes(newrooms);
+							return File(bytes, "text/plain", "rooms.yaml");
+						}
+					}
+				}
+			}
+
+			return StatusCode(StatusCodes.Status424FailedDependency, "Legacy version failed to load. DDL Error.");
+		}
     }
+	[ApiController]
+	[Route("[controller]")]
+	public class GetRooms17Controller : ControllerBase
+	{
+		private readonly ILogger<GetRooms17Controller> _logger;
+
+		public GetRooms17Controller(ILogger<GetRooms17Controller> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IActionResult Get(string s, int m, bool c, bool b, int cs, bool km, bool os)
+		{
+			FFMQRom rom = new FFMQRom();
+			var newrooms = rom.GenerateRooms(c, b, m, cs, km, os, s);
+
+			var bytes = Encoding.ASCII.GetBytes(newrooms);
+			return File(bytes, "text/plain", "rooms.yaml");
+		}
+	}
 }
